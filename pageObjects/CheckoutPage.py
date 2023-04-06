540b64cfba3d884a6245ca86f16309cab2bf9578
Gousia <gousia.sultana926@gmail.com>
2023-04-07 00:08:36 +0530

    hi this is Gousia

---
+++ pageObjects/CheckoutPage.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+
+from pageObjects.ConfirmPage import ConfirmPage
+
+
+class CheckOutPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # driver.find_elements_by_css_selector(".card-title a")
+    # driver.find_element_by_xpath("//button[@class='btn btn-success']")
+    cardTitle = (By.CSS_SELECTOR, ".card-title a")
+    cardFooter = (By.CSS_SELECTOR, ".card-footer button")
+    checkOut = (By.XPATH, "//button[@class='btn btn-success']")
+    checkout_button = (By.CSS_SELECTOR,"a[class*='btn-primary']")
+
+    def getCardTitles(self):
+        return self.driver.find_elements(*CheckOutPage.cardTitle)
+
+    def getCardFooter(self):
+        return self.driver.find_elements(*CheckOutPage.cardFooter)
+
+    def getCheckoutButton(self):
+        return self.driver.find_element(*CheckOutPage.checkout_button)
+    def checkOutItems(self):
+        self.driver.find_element(*CheckOutPage.checkOut).click()
+        confirmPage = ConfirmPage(self.driver)
+        return confirmPage
